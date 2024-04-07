249ce47f0594a0c795a5595f68ab725c61787ba9
Sumandalai <sumandalai2003@gmail.com>
2024-04-07 15:30:52 +0530

    img tag removed

--- app/page.js
+++ app/page.js
@@ -1,30 +1,32 @@
 import Feed from "@components/Feed";
-
+import Image from "next/image";
 const Home = () => (
-  <>
-  <section className="w-full h-max flex flex-col md:flex-row items-center justify-center md:justify-between">
-    <div className="flex-[0.55] desc min-[900px]:w-1/2 text-center md:text-left">
-      <h1 className="head_text">
-        Discover & Share
-        <br className="max-md:hidden" />
-        <span className="orange_gradient"> AI-Powered Prompts</span>
-      </h1>
-      <p className="desc">
-        Promptopia is an open-source AI prompting tool for the modern world to
-        discover, create, and share creative prompts.
-      </p>
+  <div className='w-full flex-center flex-col'>
+    <div className="flex gap-2">
+    <div className="lg:text-left">
+    <h1 className='head_text text-center lg:text-left'>
+      Discover & Share
+      <br className='max-md:hidden' />
+      <span className='orange_gradient text-center lg:text-left'> AI-Powered Prompts</span>
+    </h1>
+    <p className='desc text-center lg:text-left'>
+      Promptopia is an open-source AI prompting tool for modern world to
+      discover, create and share creative prompts
+    </p>
     </div>
-    <div className=" flex-[0.45] hidden min-[900px]:flex md:w-1/2 justify-center">
-      <img
-        src="/assets/images/prompmtopia_photo.jpg"
+    <div className="hidden lg:block">
+    <Image
+        src='/assets/images/prompmtopia_photo.jpg'
         alt="Image"
-        className="max-w-full h-auto md:max-w-xs rounded-md"
+        width={300}
+        height={300}
+        className="max-w-full h-auto md:max-w-xs rounded-md object-contain"
       />
     </div>
-  </section>
-  <Feed/>
-  </>
-);
+    </div>
 
+    <Feed />
+  </div>
+);
 export const revalidate = 0;
 export default Home;
